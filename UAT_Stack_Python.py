e13cdefdf55b948aa4758554e0621dff82be78bc
Kent Slack <kent.slack@gmail.com>
2019-05-16 13:24:30 -0700

    GetSize, PushNode, and Peek functions added to Stack along with associated testing

--- UAT_Stack_Python.py
+++ UAT_Stack_Python.py
@@ -18,8 +18,24 @@ class StackNode:
     
 
 class UAT_Stack:
-    def __init__():
-        pass
+    def __init__(self, top=None, size=0):
+        self.top = top
+        self.size = size
+    
+    def GetSize(self):
+        return self.size
+    
+    def PushNode(self, push_node:StackNode):
+        # Add a new node to the top of the stack
+        if(self.top == None):
+            self.top = push_node    # Stack is empty, so simply point top to the new node
+        else:
+            push_node.SetNext(top)      # Establish link between the old top node and the push_node (new node)
+            self.top = push_node        # Change the node on the top of the stack
+        
+        self.size += 1
 
 
+    def Peek(self):
+        return self.top
 
--- UAT_Stack_Python_Testing.py
+++ UAT_Stack_Python_Testing.py
@@ -31,7 +31,63 @@ def NodeTesting():
         print("FAIL - SetNext does not function correctly revise the next_node variable.")
 
 
+
+def StackTesting():
+    # Stack Constructor Testing
+    stack = UAT_Stack_Python.UAT_Stack()
+
+    if(not(stack == None)):
+        print("Success - Stack created with and references a memory address.")
+    else:
+        print("FAIL - Stack references None")
+    
+
+    # Test GetSize function
+    if(stack.GetSize() == stack.size):
+        print("Success - GetSize function reports the size value correctly.")
+    else:
+        print("FAIL - GetSize does not return the actual size of the stack.")
+
+    # Continue the testing of the constructor
+    initial_stack_size = stack.GetSize()
+    if(initial_stack_size == 0):
+        print("Success - Initial stack has a size of 0")
+    else:
+        print("FAIL - Stack size is not set to 0 as it should be for a new stack. Reported size=" + str(initial_stack_size))
+
+    # Test Peek Function
+    peek_stack = UAT_Stack_Python.UAT_Stack()
+    peek_result = peek_stack.Peek()
+    if(peek_result == None):
+        print("Success - Peek returns None on an empty stack.")
+    else:
+        print("FAIL - Peek should have returned None. Instead it returned " + peek_result)
+
+
+    # Test PushNode function
+    push_node_stack = UAT_Stack_Python.UAT_Stack()
+    node_to_push = UAT_Stack_Python.StackNode("first")
+    push_node_stack.PushNode(node_to_push)
+
+    pushnode_reported_size = push_node_stack.GetSize()
+    if(pushnode_reported_size == 1):
+        print("Success - Stack size is now 1.")
+    else:
+        print("FAIL - Stack size is not the expected size of 1. " + str(pushnode_reported_size))
+    
+    if(push_node_stack.Peek() == node_to_push):
+        print("Success - Pushed Node is at the top of the stack.")
+    else:
+        print("FAIL - Newly created node is not found on the top of the stack. Instead, it found " + push_node_stack.Peek())
+
+
+    
+
+
+
 if __name__ == "__main__":
     NodeTesting()
+
+    StackTesting()
 
 
